e85aafb3f4f6944160d321d2fe5f15707639e311
changeable848-bit <changeable848@gmail.com>
2025-09-18 21:04:37 +1200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,19 +1,10 @@
 <?php
-// Theme setup (title tag, HTML5)
-add_action('after_setup_theme', function () {
-  add_theme_support('title-tag');
-  add_theme_support('html5', ['search-form','comment-form','comment-list','gallery','caption','style','script']);
-});
-
-// Enqueue main stylesheet (cache-busted)
-add_action('wp_enqueue_scripts', function () {
-  $ver = file_exists(get_stylesheet_directory() . '/style.css') ? filemtime(get_stylesheet_directory() . '/style.css') : '1.0';
-  wp_enqueue_style('changeable-fe', get_stylesheet_uri(), [], $ver);
-});
-
-// (Perf) Remove block library CSS since we aren’t using Gutenberg on the front end
-add_action('wp_enqueue_scripts', function () {
-  wp_dequeue_style('wp-block-library');
-  wp_dequeue_style('wp-block-library-theme');
-  wp_dequeue_style('global-styles');
-}, 100);
+function changeable_enqueue_styles() {
+    wp_enqueue_style(
+        'changeable-style',
+        get_stylesheet_uri(),
+        array(),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'changeable_enqueue_styles');
