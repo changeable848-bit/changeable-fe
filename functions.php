2aafbcba72adaab154286eec3ab57609f4f8c752
changeable848-bit <changeable848@gmail.com>
2025-09-18 21:25:21 +1200

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,45 +3,24 @@
  * Changeable Front-End Theme Functions
  */
 
-// ---------------------------
 // Theme setup
-// ---------------------------
 add_action('after_setup_theme', function () {
-  // Enable title tag
   add_theme_support('title-tag');
-
-  // Enable HTML5 support
   add_theme_support('html5', [
-    'search-form',
-    'comment-form',
-    'comment-list',
-    'gallery',
-    'caption',
-    'style',
-    'script'
+    'search-form','comment-form','comment-list','gallery','caption','style','script'
   ]);
 });
 
-// ---------------------------
-// Enqueue main stylesheet
-// ---------------------------
+// Enqueue main stylesheet (cache-busted)
 add_action('wp_enqueue_scripts', function () {
   $style_path = get_stylesheet_directory() . '/style.css';
-  $ver = file_exists($style_path) ? filemtime($style_path) : '1.0';
-
-  wp_enqueue_style(
-    'changeable-fe',
-    get_stylesheet_directory_uri() . '/style.css',
-    [],
-    $ver
-  );
+  $ver = file_exists($style_path) ? filemtime($style_path) : '1.0.3';
+  wp_enqueue_style('changeable-fe', get_stylesheet_directory_uri() . '/style.css', [], $ver);
 });
 
-// ---------------------------
-// Performance: remove unused Gutenberg CSS
-// ---------------------------
+// Strip Gutenberg CSS
 add_action('wp_enqueue_scripts', function () {
   wp_dequeue_style('wp-block-library');
   wp_dequeue_style('wp-block-library-theme');
-  wp_dequeue_style('global-styles'); // theme.json frontend output
+  wp_dequeue_style('global-styles');
 }, 100);
