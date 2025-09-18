8b94df60ca6c96859a52cdb3d10ddc3514e25084
changeable848-bit <changeable848@gmail.com>
2025-09-18 21:12:31 +1200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 <?php
 ?>
 </main>
+
 <footer class="site-footer">
-  <div class="container">© Changeable — All rights reserved.</div>
+  <div>© Changeable — All rights reserved.</div>
 </footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
